11263b187565dfb284b3f3ec64685b2ce906e7c6
Smetan-dot <Smetanaandrei@gmail.com>
2023-04-02 21:02:06 +0300

    feat: add main functionality for slider at main

--- shelter/index.js
+++ shelter/index.js
@@ -25,3 +25,97 @@ navItems.forEach((element) => {
         body.classList.remove('stop_scrolling');
     })
 }) // actions for click on navigation's elements
+
+
+const arrowRight = document.querySelector('.arrow_right');
+const arrowLeft = document.querySelector('.arrow_left');
+const slides = document.querySelectorAll('.slider_wrapper');
+const wrapper = document.querySelector('.wrapper');
+const leftSlide = document.getElementById('left');
+const rightSlide = document.getElementById('right');
+let currentSlide = document.getElementById('current');
+
+async function getPets () {
+    const pets = 'pets.json';
+    const res = await fetch(pets);
+    const data = await res.json();
+    return data;
+}   // get info-cards from json
+
+function displayPets(array) {
+  array.forEach((element, index) => {
+    if(index < 3) {
+      const petCard = document.createElement('div');
+      petCard.classList.add('card');
+      petCard.innerHTML = `<img src=${element.img} alt="pet"/>
+                           <h3>${element.name}</h3>
+                           <button class="button_secondary"> Learn more </button>`;
+      slides[1].append(petCard);
+    }
+    if(index >= 3 && index < 6) {
+      const petCard = document.createElement('div');
+      petCard.classList.add('card');
+      petCard.innerHTML = `<img src=${element.img} alt="pet"/>
+                           <h3>${element.name}</h3>
+                           <button class="button_secondary"> Learn more </button>`;
+      slides[2].append(petCard);
+    }
+    if(index >= 5 && index <= 8) {
+      const petCard = document.createElement('div');
+      petCard.classList.add('card');
+      petCard.innerHTML = `<img src=${element.img} alt="pet"/>
+                           <h3>${element.name}</h3>
+                           <button class="button_secondary"> Learn more </button>`;
+      slides[0].append(petCard);
+    }  
+  })
+}
+
+function getNewSlide(array, slide) {
+    let mixData = array.slice().sort(()=>Math.random() - 0.5);
+    const sliceData = mixData.slice(0, 3);
+    sliceData.forEach((element) => {
+      const petCard = document.createElement('div');
+      petCard.classList.add('card');
+      petCard.innerHTML = `<img src=${element.img} alt="pet"/>
+                           <h3>${element.name}</h3>
+                           <button class="button_secondary"> Learn more </button>`;
+      slide.append(petCard);
+    })
+    return slide;
+}
+
+async function workSlider() {
+    const petsData = await getPets();
+    let  mixData = petsData.slice().sort(()=>Math.random() - 0.5);
+
+    displayPets(mixData);
+
+    wrapper.addEventListener("animationend", function(animationEvent) {
+        let newSlide;
+
+        if (animationEvent.animationName === "move-left") {
+            wrapper.classList.remove('transition_left');
+            currentSlide.innerHTML = leftSlide.innerHTML;
+            newSlide = leftSlide;
+        }
+        else {
+            wrapper.classList.remove('transition_right');
+            currentSlide.innerHTML = rightSlide.innerHTML;
+            newSlide = rightSlide;
+        }
+
+        newSlide.innerHTML = '';
+        newSlide = getNewSlide(mixData, newSlide);
+    })
+}
+
+workSlider();
+
+arrowRight.addEventListener('click', function() {
+    wrapper.classList.add('transition_right')
+})
+
+arrowLeft.addEventListener('click', function() {
+    wrapper.classList.add('transition_left')
+})
