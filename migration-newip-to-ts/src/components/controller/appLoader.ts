308bf366e578fab7739c0701e5290008d358ff86
Smetan-dot <smetanaandrei@gmail.com>
2023-06-07 16:49:24 +0300

    fix: add proxy

--- migration-newip-to-ts/src/components/controller/appLoader.ts
+++ migration-newip-to-ts/src/components/controller/appLoader.ts
@@ -2,7 +2,7 @@ import Loader from './loader';
 
 class AppLoader extends Loader {
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://rss-news-api.onrender.com/', {
             apiKey: '42845d8f6e3c493aba1ccf4002a5f508', // получите свой ключ https://newsapi.org/
         });
     }
