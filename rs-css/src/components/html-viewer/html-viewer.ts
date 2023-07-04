8951b300aa219edd9e0bc7cb8f6ec2d7553e0517
Smetan-dot <smetanaandrei@gmail.com>
2023-07-04 10:40:26 +0300

    refactor: fix double coding in html-viewer.ts

--- rs-css/src/components/html-viewer/html-viewer.ts
+++ rs-css/src/components/html-viewer/html-viewer.ts
@@ -25,6 +25,25 @@ export default function drawHtmlViewer (wrapper: HTMLDivElement, objects: Level[
 
 
 
+    function generateMarkupElement (element: HTMLElement, i: number, j: number): void {
+        if (objects[current].elements[i - 1][0] === 'N') {
+            markup.children[i - j].textContent = `<${objects[current].elements[i - 1].slice(1)} id="nice">`;
+            markup.children[i - j].appendChild(element);
+            const span = document.createElement('span');
+            span.textContent = `</${objects[current].elements[i - 1].slice(1)}>`;
+            markup.children[i - j].appendChild(span);
+        }
+        else {
+            markup.children[i - j].textContent = `<${objects[current].elements[i - 1]}>`;
+            markup.children[i - j].appendChild(element);
+            const span = document.createElement('span');
+            span.textContent = `</${objects[current].elements[i - 1]}>`;
+            markup.children[i - j].appendChild(span);
+        }
+    }
+
+
+    
     for (let i = 0, j = 1; i < objects[current].elements.length; i+= 1) {
         if (objects[current].elements[i]) {
             if (i === 0 || i % 2 === 0) {
@@ -40,26 +59,15 @@ export default function drawHtmlViewer (wrapper: HTMLDivElement, objects: Level[
                 }
             } // create objects in markup by even indexes in levels.elements (plate, bento)
 
+        
+
             if (i === 1 || i % 2 === 1) {
                 if (objects[current].elements[i][0] === 'S') { // symbol-coding ---> S => class="small" (look at levels.elements)
                     const el = document.createElement(`${objects[current].elements[i].slice(1)}`);
                     el.textContent = `<${objects[current].elements[i].slice(1)} class="small"></${objects[current].elements[i].slice(1)}>`;
-                    if (markup.children[i - j]) { // check parent element for current element
 
-                        if (objects[current].elements[i - 1][0] === 'N') {
-                            markup.children[i - j].textContent = `<${objects[current].elements[i - 1].slice(1)} id="nice">`;
-                            markup.children[i - j].appendChild(el);
-                            const span = document.createElement('span');
-                            span.textContent = `</${objects[current].elements[i - 1].slice(1)}>`;
-                            markup.children[i - j].appendChild(span);
-                        }
-                        else {
-                            markup.children[i - j].textContent = `<${objects[current].elements[i - 1]}>`;
-                            markup.children[i - j].appendChild(el);
-                            const span = document.createElement('span');
-                            span.textContent = `</${objects[current].elements[i - 1]}>`;
-                            markup.children[i - j].appendChild(span);
-                        }
+                    if (markup.children[i - j]) { // check parent element for current element
+                        generateMarkupElement (el, i, j);
                     }
                     else {
                         markup.appendChild(el); 
@@ -70,24 +78,8 @@ export default function drawHtmlViewer (wrapper: HTMLDivElement, objects: Level[
                     const el = document.createElement(`${objects[current].elements[i]}`);
                     el.textContent = `<${objects[current].elements[i]}></${objects[current].elements[i]}>`;
                     if (markup.children[i - j]) { // check parent element for current element
-                        
-                        if (objects[current].elements[i - 1][0] === 'N') {
-                            markup.children[i - j].textContent = `<${objects[current].elements[i - 1].slice(1)} id="nice">`;
-                            markup.children[i - j].appendChild(el);
-                            const span = document.createElement('span');
-                            span.textContent = `</${objects[current].elements[i - 1].slice(1)}>`;
-                            markup.children[i - j].appendChild(span);
-                        }
-
-                        else {
-                            markup.children[i - j].textContent = `<${objects[current].elements[i - 1]}>`;
-                            markup.children[i - j].appendChild(el);
-                            const span = document.createElement('span');
-                            span.textContent = `</${objects[current].elements[i - 1]}>`;
-                            markup.children[i - j].appendChild(span);
-                        }
+                        generateMarkupElement (el, i, j);
                     }
-
                     else {
                         markup.appendChild(el); 
                     }
