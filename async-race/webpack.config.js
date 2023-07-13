1cdb349475012806a5ca0a44a1d5eb9e92687ad2
Smetan-dot <smetanaandrei@gmail.com>
2023-07-13 11:15:06 +0300

    init: start task async-race and configurate environment

---
+++ async-race/webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+const baseConfig = {
+    entry: path.resolve(__dirname, './src/index'),
+    mode: 'development',
+    module: {
+        rules: [
+            { test: /\.ts$/i, use: 'ts-loader' },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+              },
+            {
+                test: /\.(jpg|png|svg|jpeg|gif)$/i,
+                type: 'asset/resource'
+            }
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+    },
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, '../async-race/dist'),
+        hashFunction: 'xxhash64'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' })
+    ],
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
